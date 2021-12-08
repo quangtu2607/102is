2009dbcdbb3a9841e4e66e96082dcc6cd6113702
juan Tran <tnhuan180220@gmail.com>
2021-12-08 16:37:51 +0700

    Create database for storing teams and matches

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ import requests
 import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Team(db.Model):
@@ -19,6 +20,7 @@ class Match(db.Model):
     team_a = db.Column(db.String(50), nullable=False)
     finished = db.Column(db.Boolean, nullable=False)
     started = db.Column(db.Boolean, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
 
 def clean(raw_match, teams):
     raw_match['team_h'] = teams[raw_match['team_h'] - 1]['name']
@@ -32,13 +34,12 @@ def index():
     if request.method == 'POST':
         pass
     else:
-        response = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
-        teams = response.json()['teams']
-
-        response = requests.get("https://fantasy.premierleague.com/api/fixtures/?future=1")
-        raw_matches = response.json()
-        matches = list(map(lambda x: clean(x, teams), raw_matches))
-        response = jsonify({'data': matches})
+        matches = []
+        for u in Match.query.all():
+            temp = u.__dict__
+            del temp['_sa_instance_state']
+            matches.append(temp)
+        response = jsonify(matches)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
@@ -48,4 +49,4 @@ def predict(id):
         return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80)
