dbb5793370f0f4c8f51393b4343ab50729a2073d
juan Tran <tnhuan180220@gmail.com>
2021-12-09 23:09:10 +0700

    Finished backend

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from operator import length_hint
 from re import match
 from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import requests
+import pickle
 import datetime
 
 app = Flask(__name__)
@@ -22,24 +22,11 @@ class Match(db.Model):
     started = db.Column(db.Boolean, nullable=False)
     time = db.Column(db.DateTime, nullable=False)
 
-def clean(raw_match, teams):
-    raw_match['team_h'] = teams[raw_match['team_h'] - 1]['name']
-    raw_match['team_a'] = teams[raw_match['team_a'] - 1]['name']
-    raw_match['kickoff_time'] = datetime.datetime.strptime(raw_match['kickoff_time'],"%Y-%m-%dT%H:%M:%SZ")
-    raw_match['rate'] = 'test'
-    return raw_match
-
 @app.route("/match", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         pass
     else:
-        response = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
-        teams = response.json()['teams']
-
-        response = requests.get("https://fantasy.premierleague.com/api/fixtures/?future=1")
-        raw_matches = response.json()
-        matches = list(map(lambda x: clean(x, teams), raw_matches))
         matches = []
         for u in Match.query.all():
             temp = u.__dict__
@@ -49,10 +36,19 @@ def index():
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
-@app.route("/predict/<int:id>", methods=['POST', 'GET'])
-def predict(id):
-    if request.method == 'POST':
-        return redirect('/')
+@app.route("/predict", methods=['POST'])
+def predict():
+    import numpy as np
+    with open('./static/model/randomforest.pkl', 'rb') as f:
+        load_model = pickle.load(f)
+    match_stats = request.json
+    datapoint = np.array([[match_stats['FTHG'], match_stats['FTAG'], match_stats['HS'], match_stats['AS'],\
+                           match_stats['HST'], match_stats['AST'], match_stats['HF'], match_stats['AF'],\
+                           match_stats['HC'], match_stats['AC'], match_stats['HY'], \
+                           match_stats['AY'], match_stats['HR'], match_stats['AR']]])
+    result = load_model.predict_proba(datapoint).flatten()
+    response = {'A': result[0], 'D': result[1], 'H': result[2]}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
