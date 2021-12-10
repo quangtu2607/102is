019baaab500e82c3b1acfc363b515d7fa2b9f5a6
quangtu2607 <quangtu2607@gmail.com>
2021-12-10 19:44:15 +0700

    final

--- frontend/src/components/MatchInfo.vue
+++ frontend/src/components/MatchInfo.vue
@@ -127,6 +127,7 @@ export default {
         width: 50%;
         display: grid;
         grid-template-columns: auto auto auto auto auto;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
     .grid-item {
         padding:1px;
